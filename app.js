08a339a7c3c3146c54f4b8909e75130b65131942
williampettersson <william.c.pettersson@gmail.com>
2022-09-22 16:10:51 +0200

    changed author name

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ console.log('Hello there');
 
 var array = [];
 
+var person = {
+    Name: '',
+    Number: '',
+    Address: '',
+    Email: ''
+};
+
 function submit() {
     const name = document.getElementById('name').value;
     const number = document.getElementById('number').value;
     const address = document.getElementById('address').value;
     const email = document.getElementById('email').value;
+    
     const info = document.createElement('p');
     info.innerHTML = name + ' ' + number + ' ' + address + ' ' + email;
     
